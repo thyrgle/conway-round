c55782369348d5b40a283c4be99ba32dff808452
Christopher Sumnicht <Christopher Sumnicht>
2025-08-09 08:52:33 -0700

    Minor bug with score function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import networkx as nx
 import numpy as np
-from statistics import mean
 
 
-NODES = 99
+NODES = 9
 
 
 np.set_printoptions(precision=3)
@@ -11,9 +10,10 @@ np.set_printoptions(precision=3)
 
 def score(g):
     g_mat = nx.to_numpy_array(g)
-    defective = 2 - g_mat @ g_mat
+    defective = -(g_mat @ g_mat - (2 - g_mat))
     np.fill_diagonal(defective, 0)
-    bad_count = (defective * defective).sum() / 2
+    defective = np.tri(NODES, NODES, -1).T * defective
+    bad_count = (defective ** 2).sum()
     return bad_count, defective
 
 
@@ -22,14 +22,14 @@ def construct_graph_from_genome(genome):
     return nx.from_numpy_array(g_mat)
 
 
-def adjust(genome, delta=0.001):
+def adjust(genome, delta=0.00001):
     results = []
     TRIALS = 10
     for _ in range(TRIALS):
         g = construct_graph_from_genome(genome)
         results.append((*score(g), g))
     for _, defects, g in results:
-        genome = np.clip(genome + defects * delta, 0, 1)
+        genome = np.clip(genome + defects * delta, 0.0, 1.0)
     return genome, *zip(*results)
 
 
@@ -37,16 +37,18 @@ def create_genome():
     return np.tri(NODES, NODES, -1).T * np.random.rand(NODES, NODES)
 
 
+def graph_to_genome(g):
+    return np.tri(NODES, NODES, -1).T * nx.to_numpy_array(g)
+
+
 def main():
     genome = create_genome()
-    best_avg_score = float('inf')
-    min_score = float('inf')
-    while min_score != 0:
+    best_score = float('inf')
+    while best_score != 0:
         genome, scores, _, graphs = adjust(genome)
-        if mean(scores) < best_avg_score:
-            best_avg_score = mean(scores)
-            min_score = min(scores)
-            print(min_score, best_avg_score)
+        if min(scores) < best_score:
+            best_score = min(scores)
+            print(best_score)
 
 
 if __name__ == "__main__":
