d0dda4e8641c5ee17cc23a1d710f6068dad17b24
Christopher Sumnicht <Christopher Sumnicht>
2025-08-10 06:00:37 -0700

    Changed update method.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import networkx as nx
 import numpy as np
 
 
@@ -9,8 +8,7 @@ np.set_printoptions(precision=3)
 
 
 def score(g):
-    g_mat = nx.to_numpy_array(g)
-    defective = -(g_mat @ g_mat - (2 - g_mat))
+    defective = -(g @ g - (2 - g))
     np.fill_diagonal(defective, 0)
     defective = np.tri(NODES, NODES, -1).T * defective
     bad_count = (defective ** 2).sum()
@@ -18,53 +16,39 @@ def score(g):
 
 
 def construct_graph_from_genome(genome):
-    g_mat = (genome >= np.random.rand(NODES, NODES)).astype(int)
-    return nx.from_numpy_array(g_mat)
+    g = (genome >= np.random.rand(NODES, NODES)).astype(int)
+    return g
 
 
-def adjust(genome, delta=0.00001):
-    results = []
-    TRIALS = 10
+def adjust(genome, delta=0.0005):
+    trial_results = []
+    TRIALS = 1000
     for _ in range(TRIALS):
         g = construct_graph_from_genome(genome)
-        results.append((*score(g), g))
-    for _, defects, g in results:
-        g_mat = nx.to_numpy_array(g)
-        # Select which edges should have table probability updated.
-        edge_filter = ((defects < 0) & (g_mat == 1)) | \
-                      ((defects > 0) & (g_mat == 0))
-        genome = np.clip(genome + edge_filter * defects * delta, 0.0, 1.0)
-    return genome, *zip(*results)
+        s, defects = score(g)
+        trial_results.append((s, g, defects))
+    for s, g, defects in trial_results:
+        up_g = np.tri(NODES, NODES, -1).T * (2 * g - 1)
+        genome = np.clip(genome + up_g * (1 / s) * delta, 0.0, 1.0)
+        genome = np.tri(NODES, NODES, -1).T * genome
+    return genome, *min(trial_results, key=lambda x: x[0])
 
 
 def create_genome():
-    return np.tri(NODES, NODES, -1).T * np.random.rand(NODES, NODES)
-
-
-def graph_to_genome(g, low=0.01, up=0.99):
-    return np.tri(NODES, NODES, -1).T * nx.to_numpy_array(g)
-
-
-# See https://stackoverflow.com/a/64484547/667648
-def generate_adjlist_with_all_edges(G, delimiter=" "):
-    for s, nbrs in G.adjacency():
-        line = str(s) + delimiter
-        for t, data in nbrs.items():
-            line += str(t) + delimiter
-        yield line[: -len(delimiter)]
+    return np.tri(NODES, NODES, -1).T * 0.5
 
 
 def main():
     genome = create_genome()
     best_score = float('inf')
+    best_graph = None
     while best_score != 0:
-        genome, scores, _, graphs = adjust(genome)
-        if min(scores) < best_score:
-            best_score = min(scores)
+        genome, score, graph, defects = adjust(genome)
+        if score < best_score:
+            best_score = score
+            best_graph = graph
             print(best_score)
- 
-    for line in generate_adjlist_with_all_edges(graphs[np.argmin(scores)]):
-        print(line)
+    print(best_graph)
 
 
 if __name__ == "__main__":
